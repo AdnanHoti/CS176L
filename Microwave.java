72632f936f89b5b150e1b612eefc87bb25fded45
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-10-03 17:22:14 -0400

    myMicrowave

---
+++ Microwave.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+/**
+ * @author adnanhoti
+ *
+ */
+
+//Java program to simulate the working of a Microwave
+
+//Microwave class
+
+public class Microwave {
+
+//data members time and power_level
+
+private int time;
+
+private int power_level;
+
+//constructor for initial initialization of microwave object
+
+public Microwave()
+
+{
+
+time =0;
+
+power_level =1;
+
+}
+
+//method to increase the time of microwave by 30 seconds
+
+public String time()
+
+{
+
+time += 30;
+return "Time Button was pressed. Time is " + time + " seconds.";
+
+}
+
+//method to switch the power level of microwave between 1 and 2
+
+public String power()
+
+{
+
+if(power_level == 1) {
+
+power_level =2;
+
+} else { 
+power_level = 1; }
+
+return "Power Button was pressed. Power level is " + power_level;
+
+}
+
+//method to reset the microwave
+
+public String reset()
+
+{
+
+time =0;
+
+power_level =1; 
+return "Reset Button was pressed. Power level is " + power_level + ". Time is " + time + " seconds.";
+
+}
+
+//method to start the microwave
+
+public String start()
+
+{
+
+return "Cooking for "+time+" seconds at level "+power_level;
+
+}}
---
+++ MicrowaveTester.java
@@ -0,0 +1,40 @@
+
+public class MicrowaveTester {
+	public static void main(String[] args) {
+
+			//Test the microwave class
+
+			//Create a Microwave object.
+
+			Microwave microwave = new Microwave();
+
+			
+
+			System.out.println(microwave.time());
+			
+			System.out.println(microwave.time()); 
+
+			//Set the power level to 2.
+
+			System.out.println(microwave.power());
+
+			//Start the Microwave.
+
+			System.out.println(microwave.start());
+
+			//Reset the Microwave (time 0, power level 1).
+
+			System.out.println(microwave.reset());
+
+
+		
+			
+
+
+		
+			
+
+			//end of program
+		//System.out.println("Cooking for "+time+" seconds at level "+ Power);
+	}
+}
