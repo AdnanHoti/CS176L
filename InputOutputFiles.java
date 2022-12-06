41d73c957c0e89748893801193c8321a82fd966e
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-12-05 19:04:44 -0500

    Add files via upload

--- InputOutputFiles.java
+++ InputOutputFiles.java
@@ -1,3 +1,5 @@
+package ioexception;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -8,26 +10,34 @@ import java.util.Scanner;
 */
 public class InputOutputFiles
 {
-   public static void main(String[] args) throws FileNotFoundException
-   {
-	   Scanner user = new Scanner(System.in);
-	   // Users/adnanhoti/Downloads/lines.txt
-		System.out.println("Please enter the path to open the input file for example C:\\\\Downloads\\\\lines.txt : ");
-		String userIN = user.nextLine();
-
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		final String OUTPUT_DIRECTORY = "C:\\Users\\s1320619\\eclipse-workspace\\ioexception\\src\\ioexception\\outlines.txt";
+		Scanner user = new Scanner(System.in);
+		// MAC: Users/adnanhoti/Downloads/lines.txt
+		// WINDOWS: C:\Users\s1320619\eclipse-workspace\ioexception\src\ioexception\lines.txt
+		boolean goodInput = false;
+		while (!goodInput) {
+			System.out.println("Please enter the path to open the input file for example C:\\Downloads\\lines.txt : ");
+			String userIN = user.nextLine();
+			
+			try {
+				File inputFile = new File(userIN);
+				PrintWriter outputFile = new PrintWriter(OUTPUT_DIRECTORY);
+				Scanner in = new Scanner(inputFile);
+				goodInput = true;
+				while (in.hasNext()) {
+					String what = in.nextLine();
+					outputFile.println(what);	
+				}
+				in.close();
+				outputFile.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("ERROR NOT A VALID FILE PATH");
+				goodInput = false;
+			}
+		}
 		user.close();
-      File inputFile = new File(userIN);
-      PrintWriter outputFile = new PrintWriter("src/outlines.txt");
-     
-      Scanner in = new Scanner(inputFile);
-      while (in.hasNext()) 
-      {
-      String what =in.nextLine();
-      outputFile.println(what);
-      }
-      in.close();
-      outputFile.close();
-      System.out.println("complete");
-      
-   }
+		System.out.println("complete");  
+	}
 }
